06a9706cbeacd99921fea051c5691aea651bdd2e
Vinicius Bretzke <vinicius@createsoftware.com.br>
2023-06-06 14:45:01 -0300

    First commit, creating application and dependencies.

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, Text, View } from "react-native";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+
+import blurBg from "./src/assets/bg-blur.png";
+
+export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
+  return (
+    <ImageBackground 
+      source={blurBg} 
+      className="flex-1 items-center justify-center bg-gray-900" 
+      imageStyle={{position: 'absolute', left: '-100%'}}
+    >      
+      <StatusBar style="light" translucent />
+    </ImageBackground>
+  );
+}
