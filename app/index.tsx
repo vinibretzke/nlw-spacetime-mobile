f7855eecfbcb4ea6a4f0a17896f6dd244eec7c52
Vinicius Bretzke <vinicius@createsoftware.com.br>
2023-06-30 17:09:29 -0300

    latest changes for mobile

--- app/index.tsx
+++ app/index.tsx
@@ -1,80 +1,82 @@
-import { useEffect } from "react";
-import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
-import * as SecureStore from "expo-secure-store";
-import { styled } from "nativewind";
-import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import { useEffect } from 'react'
+import { useRouter } from 'expo-router'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
+import { styled } from 'nativewind'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
+} from '@expo-google-fonts/roboto'
 
-import blurBg from "../src/assets/bg-blur.png";
-import Stripes from "../src/assets/stripes.svg";
-import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
-import { api } from "../src/lib/api";
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
+import { api } from '../src/lib/api'
 
-const StyledStripes = styled(Stripes);
+const StyledStripes = styled(Stripes)
 
 const discovery = {
-  authorizationEndpoint: "https://github.com/login/oauth/authorize",
-  tokenEndpoint: "https://github.com/login/oauth/access_token",
+  authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+  tokenEndpoint: 'https://github.com/login/oauth/access_token',
   revocationEndpoint:
-    "https://github.com/settings/connections/applications/2334608761e437dad3a4",
-};
+    'https://github.com/settings/connections/applications/2334608761e437dad3a4',
+}
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
-  });
+  })
 
   const [, response, signInWithGithub] = useAuthRequest(
     {
-      clientId: "2334608761e437dad3a4",
-      scopes: ["identity"],
+      clientId: '2334608761e437dad3a4',
+      scopes: ['identity'],
       redirectUri: makeRedirectUri({
-        scheme: "nlwspacetime",
+        scheme: 'nlwspacetime',
       }),
     },
     discovery,
-  );
+  )
 
+  async function handleGithubOauthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+
+    const { token } = response.data
+
+    await SecureStore.setItemAsync('token', token)
+    router.push('/memories')
+  }
   useEffect(() => {
     // console.log(
     //   makeRedirectUri({
     //     scheme: 'nlwspacetime',
     //   }),
     // )
-    if (response?.type === "success") {
-      const { code } = response.params;
-      api
-        .post("/register", {
-          code,
-        })
-        .then((response) => {
-          const { token } = response.data;
+    if (response?.type === 'success') {
+      const { code } = response.params
 
-          SecureStore.setItemAsync("token", token);
-          console.log(token);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      handleGithubOauthCode(code)
     }
-  }, [response]);
+  }, [response])
 
   if (!hasLoadedFonts) {
-    return null;
+    return null
   }
 
   return (
     <ImageBackground
       source={blurBg}
       className="flex-1 items-center justify-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: "absolute", left: "-100%" }}
+      imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2" />
       <View className="flex-1 items-center justify-center gap-6">
@@ -106,5 +108,5 @@ export default function App() {
       </Text>
       <StatusBar style="light" translucent />
     </ImageBackground>
-  );
+  )
 }
